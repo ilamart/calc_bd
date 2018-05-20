18819a3f9bfde32e36ba7a621bc556f44bb0e9e5
ilamart <39222202+ilamart@users.noreply.github.com>
2018-05-20 11:50:29 +0300

    Initial commit

---
+++ calc_bd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc_bd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите выражение: "); //Предлагаем ввести выражение
+            string exp = Console.ReadLine();
+            Console.WriteLine("{0} = {1}", exp, new System.Data.DataTable().Compute(exp, ""));
+            Console.ReadKey();
+        }
+    }
+}
